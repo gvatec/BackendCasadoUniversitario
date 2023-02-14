636f1e639a7e761a8b31221ff5c60e0c176bf3cd
gvatec <tecwebcod@gmail.com>
2023-02-14 19:27:50 -0300

    criando service users nao terminado ainda

--- src/Services/Users/UsersCreateService.ts
+++ src/Services/Users/UsersCreateService.ts
@@ -1,2 +1,38 @@
 import prismaclient from "../../Prisma/Prismacliente";
 
+interface Usercredencials {
+  email: string;
+  name: string;
+  datanascimento: Date;
+  telefone: number;
+  password: string;
+}
+
+class UserCreateService {
+  async execute({
+    name,
+    email,
+    telefone,
+    datanascimento,
+    password,
+  }: Usercredencials) {
+    const usercreate = await prismaclient.users.create({
+      data: {
+        name: name,
+        email: email,
+        telefone: telefone,
+        datanascimento: datanascimento,
+        password: password,
+      },
+      select: {
+        id: true,
+        email: true,
+        name: true,
+        telefone: true,
+      },
+    });
+    return usercreate;
+  }
+}
+
+export { UserCreateService };
--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,7 @@ import route from './routes'
 
 const app = express()
 
+app.use(express.json())
 app.use(cors())
 app.use(route)
 
