25a0d191c07b0986755f666d1f90fa835609435d
gvatec <tecwebcod@gmail.com>
2023-02-20 08:29:31 -0300

    arrumando db

--- prisma/migrations/20230220112911_admin/migration.sql
+++ prisma/migrations/20230220112911_admin/migration.sql
@@ -12,3 +12,17 @@ CREATE TABLE `users` (
     UNIQUE INDEX `users_id_key`(`id`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `session` (
+    `iduser` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `telefone` VARCHAR(191) NOT NULL,
+    `datanascimento` VARCHAR(191) NOT NULL,
+    `created_at` DATETIME(3) NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    UNIQUE INDEX `session_iduser_key`(`iduser`),
+    PRIMARY KEY (`iduser`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
